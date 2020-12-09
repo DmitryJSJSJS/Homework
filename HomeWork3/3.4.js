61d54facd56d97bbd33810209af742a768987b8b
Dmitry Shabunya <bulbasaurus01@gmail.com>
2020-12-09 18:10:34 +0300

    HomeWork3

--- HomeWork3/3.4.js
+++ HomeWork3/3.4.js
@@ -3,43 +3,25 @@ var familyname, firstname, secondname, ageyear, ageday, ageyearplus, sex, sex1, 
 do{
     familyname = prompt ('Ваша фамилия');
 }
-while (familyname === null);
-if (familyname === null){
-    familyname === undefined;
-}
-familyname = ('Некорректный ввод');
+while (!familyname);
 
 do{
-    firstname = prompt ('Ваш имя');
+    firstname = prompt ('Ваше имя');
 }
-while (firstname === null);
-if (firstname === null){
-    firstnamename === undefined;
-}
-familyname = ('Некорректный ввод');
+while (!firstname);
 
 do{
     secondname = prompt ('Ваше отчество');
 }
-while (secondname === null);
-if (secondname === null){
-    secondname === undefined;
-}
-familyname = ('Некорректный ввод');
+while (!secondname);
 
 do{
     ageyear = +prompt ('Ваш возраст');
 }
-while (ageyear <= 0);
-if (ageyear/ageyear !== 1){
-    ageyear = ('Некорректный ввод');
-}
-if (ageyear/ageyear !== 1){
-    ageday = ageyear * 365;
-    ageyearplus = ageyear + 5;
-}
-ageday = ('Некорректный ввод');
-ageyearplus = ('Некорректный ввод');
+while (ageyear <= 0 && ageyear/ageyear !== 1);
+
+ageday = ageyear * 365;
+ageyearplus = ageyear + 5;
 
 sex = confirm ('Ваш пол - мужской?');
 
@@ -62,6 +44,5 @@ else if (sex === false && ageyear < 58){
 else if (sex === false && ageyear >= 58){
     p = ('Да')
 }
-else p = ('Неизвестно из-за некорректного ввода');
 
 alert ('ФИО:' +' ' +familyname +' ' +firstname +' ' +secondname +' ' +'Ваш возраст в годах:' +' ' +ageyear +' ' +'Ваш возраст в днях:' +' ' +ageday +' ' +'Через 5 лет Вам будет:' +' ' +ageyearplus +' ' +'Ваш пол:' +' ' +sex1 +' ' +'Вы на пенсии: ' +p);
